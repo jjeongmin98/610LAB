3d5750f262dd3e743c1b7cd24ab7b696d8ea001f
jjeongmin98 <87534918+jjeongmin98@users.noreply.github.com>
2022-06-09 18:59:39 +0900

    Delete node.js

--- node.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const os = require('os');
-console.log("Test server starting...");
-var handler = function(req, res){
-        res.writeHead(200);
-        res.end("Container Hostname:" + os.hostname() + "\n");
-};
-var www = http.createServer(handler);
-www.listen(8080);
